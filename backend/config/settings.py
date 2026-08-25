5e9b62e80596aef141f51a2a7fbc0d17190cb218
silamaka <diallosila20@gmail.com>
2026-08-25 18:57:59 +0200

    modifier du settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("DB_NAME", "project_harmony"),
         "USER": os.environ.get("DB_USER", "project_harmony"),
-        "PASSWORD": os.environ["DB_PASSWORD"],
+        "PASSWORD": os.environ.get("DB_PASSWORD", ""),
         "HOST": os.environ.get("DB_HOST", "localhost"),
         "PORT": os.environ.get("DB_PORT", "5432"),
     }
